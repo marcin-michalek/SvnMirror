7488998e42056a838b75009a1061214ec1f257e2
Marcin Michałek <marcin.michalek@miquido.com>
2015-06-12 12:46:32 +0200

    Project init, Swing hello world

---
+++ src/main/java/com/svn/utils/mirror/SvnMirror.java
@@ -0,0 +1,13 @@
+package com.svn.utils.mirror;
+
+import com.svn.utils.mirror.gui.MainWindow;
+
+/**
+ * SvnMirror
+ * Created by Marcin on 2015-06-12.
+ */
+public class SvnMirror {
+    public static void main(String[] args) {
+        new MainWindow("HelloWorldSwing");
+    }
+}
